0c3f5cb5d476807a42701cf433783ad61e46dd6f
lyndascode <lyndaironhack@gmail.com>
2025-06-13 01:42:41 +0200

    Update navbar.jsx
    show login/signup only when logged out, and show profile when logged in

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,13 +1,13 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
 import '../Navbar/navbar.css';
 import ProfileDropdown from './ProfileDropdown';
-function handleLogout() {
-    localStorage.removeItem("authToken");
-    window.location.href = "/login";
-}
+import { AuthContext } from "../../context/auth.context"; //   import AuthContext
 
 function Navbar() {
+    const { isLoggedIn, user, logOutUser, isAdmin } = useContext(AuthContext); // ✅ access auth state
+
     return (
         <nav className="navbar">
             <div className="navbar-left">
@@ -17,16 +17,24 @@ function Navbar() {
             <div className="navbar-links">
                 <Link to="/concerts">Concerts</Link>
                 <Link to="/artists">Artists</Link>
-                <Link to="/signup">Signup</Link>
-                <Link to="/login">Login</Link>
 
-                <div className="navbar-right">
-                    <ProfileDropdown />
-                </div>
+                {/*   Show if user isnt logged in */}
+                {!isLoggedIn && (
+                    <>
+                        <Link to="/signup">Signup</Link>
+                        <Link to="/login">Login</Link>
+                    </>
+                )}
+
+                {/*   Show only when user is logged in */}
+                {isLoggedIn && (
+                    <div className="navbar-right">
+                        <ProfileDropdown />
+                    </div>
+                )}
             </div>
         </nav>
     );
 }
 
 export default Navbar;
-
